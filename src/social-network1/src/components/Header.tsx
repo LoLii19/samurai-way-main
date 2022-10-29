6d207c475abc39cd0c027a6145d51960993c6963
LoLii19 <cozlowskaya2010@yandex.ru>
2022-10-29 05:48:57 +0300

    The beging of the project "social network". Tasks from samurai 11-12

---
+++ src/social-network1/src/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Header() {
+    return(
+        <header className='header'>
+            <img src='https://www.designmantic.com/logo-images/172643.png?company=Company+Name&slogan=&verify=1'/>
+        </header>
+    )
+}
+export default Header;
