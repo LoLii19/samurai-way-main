7b985dd8d87d67564360c4ae0afccaa6e055f9fb
LoLii19 <cozlowskaya2010@yandex.ru>
2022-10-29 05:41:50 +0300

    tasks from samurai way(0-9) part 2

--- src/samurai_0-9/App.tsx
+++ src/samurai_0-9/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import Tecnologies from './Tecnologies';
-import Header from './Header';
+import Header from "./Header";
+import Footer from "./Footer";
+
 
 function App() {
   return (
       <div>
+          <Footer />
       <Header />
         <Tecnologies />
       </div>
