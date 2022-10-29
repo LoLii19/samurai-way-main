5be76e11b9728e711ef65ebbead8b7a7bdcc3050
LoLii19 <cozlowskaya2010@yandex.ru>
2022-10-29 06:05:06 +0300

    Change routing of the project "samurau-way-main" to "social network".

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './samurai_0-9/App';
+// import App from './samurai_0-9/App';
+import App from './social-network1/App';
 
 ReactDOM.render(
     <App />,
